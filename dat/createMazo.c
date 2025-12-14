1cb5001db42e87fd2ba1de6596947ec37f9f8c1a
gaston <gastonleonelgauna2@gmail.com>
2025-12-14 00:14:54 -0300

    agregue el ver mazo desde mazo.c desde el vector

--- dat/createMazo.c
+++ dat/createMazo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/common.h"
+#include "../include/common.h"
 
 FILE *archivoMazo;
 
@@ -16,38 +16,40 @@ void crearMazo(){
 
     for (Palo palo = ESPADAS; palo <= COPAS; palo++) {
         for (int num = 1; num <= 12; num++) {
-            if (palo == ESPADAS && num == 1) {
-                jerarquia = 14;
-            }else if (palo == BASTOS && num == 1) {
-                jerarquia = 13;
-            }else if (palo == ESPADAS && num == 7) {
-                jerarquia = 12;
-            }else if (palo == OROS && num == 7){
-                jerarquia = 11;                
-            }else if (num == 3) {
-                jerarquia = 10;
-            }else if (num == 2) {
-                jerarquia = 9;
-            }else if ((palo == OROS || palo == COPAS) && num == 1) {
-                jerarquia = 8;
-            }else{
-                jerarquia = num;
-            }
+            if(num != 8 && num != 9){
+                            if (palo == ESPADAS && num == 1) {
+                            jerarquia = 14;
+                        }else if (palo == BASTOS && num == 1) {
+                            jerarquia = 13;
+                        }else if (palo == ESPADAS && num == 7) {
+                            jerarquia = 12;
+                        }else if (palo == OROS && num == 7){
+                            jerarquia = 11;                
+                        }else if (num == 3) {
+                            jerarquia = 10;
+                        }else if (num == 2) {
+                            jerarquia = 9;
+                        }else if ((palo == OROS || palo == COPAS) && num == 1) {
+                            jerarquia = 8;
+                        }else{
+                            jerarquia = num;
+                        }
 
-            
-            nuevaCarta.num = num;
-            nuevaCarta.palo = palo;
-            nuevaCarta.jerarquia = jerarquia;
-            if(num == 12 || num == 11 || num == 10) {
-                nuevaCarta.puntos = 0;
-            }else{
-                nuevaCarta.puntos = num;
+                        
+                        nuevaCarta.num = num;
+                        nuevaCarta.palo = palo;
+                        nuevaCarta.jerarquia = jerarquia;
+                        if(num == 12 || num == 11 || num == 10) {
+                            nuevaCarta.puntos = 0;
+                        }else{
+                            nuevaCarta.puntos = num;
+                        }
+                        printf("Carta creada: Num %d, Palo %d, Jerarquia %d, Puntos %d\n", nuevaCarta.num, nuevaCarta.palo, nuevaCarta.jerarquia, nuevaCarta.puntos);
+                        printf("==============================================================\n");
+                        fwrite(&nuevaCarta, sizeof(carta), 1, archivoMazo);
             }
             
-
-            fwrite(&nuevaCarta, sizeof(carta), 1, archivoMazo);
-            printf("Carta creada: Num %d, Palo %d, Jerarquia %d, Puntos %d\n", nuevaCarta.num, nuevaCarta.palo, nuevaCarta.jerarquia, nuevaCarta.puntos);
-            printf("==============================================================\n");
+            
         }
 
         printf("\n\n\n");
--- src/mazo.c
+++ src/mazo.c
@@ -2,38 +2,43 @@
 #include <stdlib.h>
 #include "../include/common.h"
 
-
-
-
 typedef carta tMazo[40];
 
-void extraerMazo(tMazo *pMazo);
+
+int extraerMazo(tMazo pMazo);
+void verMazo(tMazo pMazo, int n);
 
 int main() {
     tMazo mazo;
-    extraerMazo(&mazo);
+    //inicializarMazo(mazo);
+    int n = extraerMazo(mazo);
+    verMazo(mazo, n);
     
     return 0;
 }
 
 
 //ESTA FUNCION EXTRAE EL MAZO DE CARTAS DEL ARCHIVO BINARIO 
-void extraerMazo(tMazo *pMazo) {
+int extraerMazo(tMazo pMazo) {
     FILE *archivo;
     carta pCarta;
     int i = 0;
-    archivo = fopen("../dat/mazo.dat", "rb");
+    archivo = fopen("../../dat/mazo.dat", "rb");
     if (archivo == NULL) {
-        printf("Error al abrir el archivo");
-        return;
+        perror("Error al abrir el archivo");
+        return 0;
     }
-    fread(&pCarta,sizeof(carta),1,archivo);
-    while (!feof(archivo)){
-        (*pMazo)[i] = pCarta;
-        i++;
-        fread(&pCarta,sizeof(carta),1,archivo);
+    while (i < 40 && fread(&pCarta, sizeof(carta), 1, archivo) == 1) {
+        pMazo[i++] = pCarta;
     }
-    
 
     fclose(archivo);
+    return i;
+}
+
+//haceme una funcion para ver el mazo de cartas por pantalla
+void verMazo(tMazo pMazo, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Carta %d: Num: %d, Palo: %d, Jerarquia: %d, Puntos: %d\n\n", i+1, pMazo[i].num, pMazo[i].palo, pMazo[i].jerarquia, pMazo[i].puntos);
+    }
 }
